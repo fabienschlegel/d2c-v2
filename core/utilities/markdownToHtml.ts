7d2fe5878901405323b41a3297c46a6c011cbc86
Fabien Schlegel <fabien.schlegel.fr@gmail.com>
2022-10-11 17:48:42 +0000

    Fix/img in markdown

--- core/utilities/markdownToHtml.ts
+++ core/utilities/markdownToHtml.ts
@@ -1,9 +1,12 @@
 import { remark } from "remark";
 import html from "remark-html";
 import prism from "remark-prism";
+import remarkUnwrapImages from "remark-unwrap-images";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
+    // * Remove p tag around img
+    .use(remarkUnwrapImages)
     .use(html, { sanitize: false })
     .use(prism)
     .process(markdown);
